5e9208ab385d9f273b715c446d96f86bf9f57e33
Sarankp <47633011+Sarankp@users.noreply.github.com>
2020-11-06 09:24:14 +0530

    Update IBillPaymentServiceTest.java

--- src/com/cg/paymentapp/Test/IBillPaymentServiceTest.java
+++ src/com/cg/paymentapp/Test/IBillPaymentServiceTest.java
@@ -23,19 +23,115 @@ class IBillPaymentServiceTest {
 		
 	}
 		@Test
-		public void addBillPaymentTest(){
+		public void addBillPaymentTestdth(){
 			BillPayment payment = new BillPayment();
 			payment.setAmount(399);
 			payment.setBilltype(BillType.DTH);
-			payment.setPaymentDate(LocalDate.parse("2020-11-04"));
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
 			assertEquals(payment,billpayment.addBillPayment(payment));
 		}
 		@Test
-		public void viewBillPaymentTest(){
+		public void viewBillPaymentTestdth(){
 			BillPayment payment = new BillPayment();
 			payment.setAmount(399);
 			payment.setBilltype(BillType.DTH);
-			payment.setPaymentDate(LocalDate.parse("2020-11-04"));
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
+			assertEquals(payment,billpayment.viewBillPayment(payment));
+		
+		}
+		
+		
+		
+		@Test
+		public void addBillPaymentTestMpre(){
+			BillPayment payment = new BillPayment();
+			payment.setAmount(399);
+			payment.setBilltype(BillType.MobilePrepaid);
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
+			assertEquals(payment,billpayment.addBillPayment(payment));
+		}
+		@Test
+		public void viewBillPaymentTestMpre(){
+			BillPayment payment = new BillPayment();
+			payment.setAmount(399);
+			payment.setBilltype(BillType.MobilePrepaid);
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
+			assertEquals(payment,billpayment.viewBillPayment(payment));
+		
+		}
+		
+		
+		@Test
+		public void addBillPaymentTestMpo(){
+			BillPayment payment = new BillPayment();
+			payment.setAmount(399);
+			payment.setBilltype(BillType.MobilePostpaid);
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
+			assertEquals(payment,billpayment.addBillPayment(payment));
+		}
+		@Test
+		public void viewBillPaymentTestMpo(){
+			BillPayment payment = new BillPayment();
+			payment.setAmount(399);
+			payment.setBilltype(BillType.MobilePostpaid);
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
+			assertEquals(payment,billpayment.viewBillPayment(payment));
+		
+		}
+		
+		
+		@Test
+		public void addBillPaymentTestCC(){
+			BillPayment payment = new BillPayment();
+			payment.setAmount(399);
+			payment.setBilltype(BillType.CreditCard);
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
+			assertEquals(payment,billpayment.addBillPayment(payment));
+		}
+		@Test
+		public void viewBillPaymentTestCC(){
+			BillPayment payment = new BillPayment();
+			payment.setAmount(399);
+			payment.setBilltype(BillType.CreditCard);
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
+			assertEquals(payment,billpayment.viewBillPayment(payment));
+		
+		}
+		
+		
+		@Test
+		public void addBillPaymentTestLIC(){
+			BillPayment payment = new BillPayment();
+			payment.setAmount(399);
+			payment.setBilltype(BillType.LICPremium);
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
+			assertEquals(payment,billpayment.addBillPayment(payment));
+		}
+		@Test
+		public void viewBillPaymentTestLIC(){
+			BillPayment payment = new BillPayment();
+			payment.setAmount(399);
+			payment.setBilltype(BillType.LICPremium);
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
+			assertEquals(payment,billpayment.viewBillPayment(payment));
+		
+		}
+		
+		
+		@Test
+		public void addBillPaymentTestLPG(){
+			BillPayment payment = new BillPayment();
+			payment.setAmount(399);
+			payment.setBilltype(BillType.LPG);
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
+			assertEquals(payment,billpayment.addBillPayment(payment));
+		}
+		@Test
+		public void viewBillPaymentTestLPG(){
+			BillPayment payment = new BillPayment();
+			payment.setAmount(399);
+			payment.setBilltype(BillType.LPG);
+			payment.setPaymentDate(LocalDate.parse("2020-11-05"));
 			assertEquals(payment,billpayment.viewBillPayment(payment));
 		
 		}
